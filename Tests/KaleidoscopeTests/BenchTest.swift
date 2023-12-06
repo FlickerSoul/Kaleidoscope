83d9363ba3c63e1a7bd741ee4107a0efb03c8044
Heyuan Zeng <hi@universe.observer>
2023-12-06 22:25:28 +0000

    test: add benchmarks from logos

--- Tests/KaleidoscopeTests/BenchTest.swift
+++ Tests/KaleidoscopeTests/BenchTest.swift
@@ -10,10 +10,10 @@ import XCTest
 
 @kaleidoscope(skip: "\t| |\n")
 enum BenchTestToken {
-    @regex(#"[a-zA-Z_$][a-zA-Z0-9_$]*+"#)
+    @regex(#"[a-zA-Z_$][a-zA-Z0-9_$]*?"#)
     case Identifier
 
-    @regex(#""([^"\\]|\t|\n|\")*+""#)
+    @regex(#""([^"\\]|\\t|\\n|\\n|\\")*?""#)
     case String
 
     @token(#"private"#)
@@ -69,7 +69,7 @@ enum BenchTestToken {
 }
 
 let SOURCE = """
-foobar (protected primitive private instanceof in) { + ++ = == === => }
+foobar(protected primitive private instanceof in) { + ++ = == === => }
 foobar(protected primitive private instanceof in) { + ++ = == === => }
 foobar(protected primitive private instanceof in) { + ++ = == === => }
 foobar(protected primitive private instanceof in) { + ++ = == === => }
@@ -120,17 +120,40 @@ It was the year when they finally immanentized the Eschaton
 let STRINGS = #""tree" "to" "a" "graph" "that can" "more adequately represent" "loops and arbitrary state jumps" "with\"\"\"out" "the\n\n\n\n\n" "expl\"\"\"osive" "nature\"""of trying to build up all possible permutations in a tree." "tree" "to" "a" "graph" "that can" "more adequately represent" "loops and arbitrary state jumps" "with\"\"\"out" "the\n\n\n\n\n" "expl\"\"\"osive" "nature\"""of trying to build up all possible permutations in a tree." "tree" "to" "a" "graph" "that can" "more adequately represent" "loops and arbitrary state jumps" "with\"\"\"out" "the\n\n\n\n\n" "expl\"\"\"osive" "nature\"""of trying to build up all possible permutations in a tree." "tree" "to" "a" "graph" "that can" "more adequately represent" "loops and arbitrary state jumps" "with\"\"\"out" "the\n\n\n\n\n" "expl\"\"\"osive" "nature\"""of trying to build up all possible permutations in a tree.""#
 
 let BENCH_SOURCE = [SOURCE, IDENTIFIERS, STRINGS]
+let TOKEN_NUMs = [30 * 16, 13 * 10, 48]
 
 final class TestBenchMark: XCTestCase {
-    func testBench() throws {
-        for benchSource in BENCH_SOURCE {
-            let startTime = Date()
-            let tokens = Array(BenchTestToken.lexer(source: benchSource).spannedAndSliced)
-            let endTime = Date()
+    func testBenchCorrect() throws {
+        for (index, (benchSource, tokenNum)) in zip(BENCH_SOURCE, TOKEN_NUMs).enumerated() {
+            XCTContext.runActivity(named: "test parsing the \(index)th test with \(tokenNum) tokens") { _ in
+                let startTime = Date()
+                let tokens = Array(BenchTestToken.lexer(source: benchSource))
+                let endTime = Date()
 
-            let elapsedTime = endTime.timeIntervalSince(startTime)
+                let elapsedTime = endTime.timeIntervalSince(startTime)
 
-            print("parsing \(benchSource.count) in \(elapsedTime) seconds: \(Double(benchSource.unicodeScalars.count) / Double(elapsedTime)) scalar/s")
+                _ = tokens.map { try! $0.get() }
+
+                XCTAssertEqual(tokens.count, tokenNum, "token num of the \(index)th test did not match")
+            }
+        }
+    }
+
+    func testBenchSpeed() throws {
+        let ITERATION_COUNT = 10
+
+        for (index, (benchSource, tokenNum)) in zip(BENCH_SOURCE, TOKEN_NUMs).enumerated() {
+            XCTContext.runActivity(named: "test parsing the \(index)th test with \(tokenNum) tokens") { _ in
+                let startTime = Date()
+                for _ in 0 ..< ITERATION_COUNT {
+                    _ = Array(BenchTestToken.lexer(source: benchSource).map { $0.get })
+                }
+                let endTime = Date()
+
+                let elapsedTime = endTime.timeIntervalSince(startTime)
+
+                print("parsing \(benchSource.count) for \(ITERATION_COUNT) iterations in \(elapsedTime) seconds: \(Double(benchSource.unicodeScalars.count * ITERATION_COUNT) / Double(elapsedTime)) scalar/s")
+            }
         }
     }
 }
