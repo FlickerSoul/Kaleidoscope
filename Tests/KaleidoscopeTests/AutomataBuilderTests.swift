19e7dbdcadf9c11001f1153c5d7e27e8d2e38e80
Heyuan Zeng <hi@universe.observer>
2023-12-22 16:07:28 +0000

    test: graph generation

--- Tests/KaleidoscopeTests/AutomataBuilderTests.swift
+++ Tests/KaleidoscopeTests/AutomataBuilderTests.swift
@@ -8,6 +8,35 @@
 @testable import KaleidoscopeMacros
 import XCTest
 
+protocol IntoByte {
+    func into() -> UInt32
+}
+
+extension Character: IntoByte {
+    func into() -> UInt32 {
+        return self.scalarByte
+    }
+}
+
+extension String: IntoByte {
+    func into() -> UInt32 {
+        assert(self.count == 1)
+        return self.first!.scalarByte
+    }
+}
+
+extension UInt32: IntoByte {
+    func into() -> UInt32 {
+        return self
+    }
+}
+
+extension Int: IntoByte {
+    func into() -> UInt32 {
+        return UInt32(self)
+    }
+}
+
 func branch(_ children: [Character: NodeId], _ miss: NodeId? = nil, inverted: Bool = false) -> Node {
     return .Branch(.init(branches: Dictionary(uniqueKeysWithValues: children.map { ($0.scalarByte.scalarByteRange, $1) }), miss: miss))
 }
@@ -16,8 +45,12 @@ func branch(_ children: [Node.BranchHit: NodeId], _ miss: NodeId? = nil, inverte
     return .Branch(.init(branches: children, miss: miss))
 }
 
-func range(_ lhs: Character, _ rhs: Character) -> Node.BranchHit {
-    return lhs.scalarByte...rhs.scalarByte
+func seq(_ seq: String, _ then: NodeId, _ miss: Node.SeqMiss? = nil) -> Node {
+    return .Seq(.init(seq: seq.map { $0.into() }, then: then, miss: miss))
+}
+
+func range(_ lhs: any IntoByte, _ rhs: any IntoByte) -> Node.BranchHit {
+    return lhs.into()...rhs.into()
 }
 
 func leaf(_ end: EndsId) -> Node {
@@ -29,27 +62,27 @@ final class GraphTests: XCTestCase {
         let tests: [(regexs: [String], nodes: [Node?], root: NodeId)] = [
             (
                 regexs: ["ab"],
-                nodes: [branch(["a": 1]), branch(["b": 2]), leaf(0)],
+                nodes: [seq("ab", 1), leaf(0)],
                 root: 0
             ),
             (
                 regexs: ["ab", "ab(b)+?"],
-                nodes: [branch(["b": 3], 5), branch(["b": 0]), branch(["a": 1]), branch(["b": 3], 4), leaf(1), leaf(0)],
+                nodes: [seq("b", 3, .first(5)), branch(["b": 0]), branch(["a": 1]), seq("b", 3, .first(4)), leaf(1), leaf(0)],
                 root: 2
             ),
             (
                 regexs: ["ab", "[a-b]+?"],
-                nodes: [branch([range("a", "b"): 3], 5), branch(["a": 3, "b": 0], 4), branch(["a": 1, "b": 3]), branch([range("a", "b"): 3], 4), leaf(1), leaf(0)],
+                nodes: [branch([range("a", "b"): 3], 5), seq("b", 0, .anytime(3)), branch(["a": 1, "b": 3]), branch([range("a", "b"): 3], 4), leaf(1), leaf(0)],
                 root: 2
             ),
             (
                 regexs: ["ab", "[^a]"],
-                nodes: [branch([range("a", "a"): 2, range("b", Character(Unicode.Scalar(HIR.ScalarByte.max)!)): 1, range(Character(Unicode.Scalar(0)), "`"): 1]), leaf(1), branch(["b": 3]), leaf(0)],
-                root: 0
+                nodes: [seq("b", 3), branch([0...96: 2, 98...HIR.ScalarByte.max: 2, 97...97: 0]), leaf(1), leaf(0)],
+                root: 1
             ),
             (
                 regexs: ["ab", "[^bc]+?"],
-                nodes: [branch([0...97: 2, 98...98: 4, 100...65535: 2], 3), branch([97...97: 0, 100...65535: 2, 0...96: 2]), branch([0...97: 2, 100...65535: 2], 3), leaf(1), leaf(0)],
+                nodes: [branch([0...97: 2, 98...98: 4, 100...HIR.ScalarByte.max: 2], 3), branch([97...97: 0, 100...HIR.ScalarByte.max: 2, 0...96: 2]), branch([0...97: 2, 100...HIR.ScalarByte.max: 2], 3), leaf(1), leaf(0)],
                 root: 1
             )
         ]
